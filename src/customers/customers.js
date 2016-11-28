a704f142d1dbab63a5b5a583cf81c9411d7429c4
Chad Kruszynsky <ckruszynsky@turner-industries.com>
2016-11-28 14:56:42 -0600

    fixed bug with disabled and firing click events

--- src/customers/customers.js
+++ src/customers/customers.js
@@ -1,58 +1,67 @@
 import {inject} from 'aurelia-framework';
 import {CustomerService} from './customer-service';
 
-
 @inject(CustomerService)
 export class Customers {
   customers = [];
-  pages = [];  
-  pageSize = 10;  
+  pages = [];
+  pageSize = 10;
   currentPage = [];
   currentPageIndex = 0;
-  
+
   constructor(customerService) {
     this.customerService = customerService;
   }
 
   activate() {
     return Promise.all([
-      this.customerService .getCustomers().then(message => {
+      this
+        .customerService
+        .getCustomers()
+        .then(message => {
           var resp = JSON.parse(message.response);
-          this.customers = resp.Customers;  
+          this.customers = resp.Customers;
           this.setupPaging(this.customers);
-         //n, (pageSize * n + 1)  -1
+          //n, (pageSize * n + 1)  -1
         })
-    ]);    
+    ]);
   }
 
-  setupPaging(data){
-     this.pages = _.range(0,data.length, this.pageSize);
-     this.goToPage(0);
+  setupPaging(data) {
+    this.pages = _.range(0, data.length, this.pageSize);
+    this.goToPage(0);
   }
 
-  goToPage(pageIndex){
+  goToPage(pageIndex) {
     this.currentPageIndex = pageIndex;
     let minIndex = this.pages[pageIndex];
     let maxIndex = minIndex + this.pageSize - 1;
-    this.currentPage = this.customers.slice(minIndex,maxIndex);
+    this.currentPage = this
+      .customers
+      .slice(minIndex, maxIndex);
   }
   goToPreviousPage() {
-    this.currentPageIndex = this.currentPageIndex - 1;
-    this.goToPage(this.currentPageIndex);
+    if (this.currentPageIndex !== 0) {
+      this.currentPageIndex = this.currentPageIndex - 1;
+      this.goToPage(this.currentPageIndex);
+    }
   }
 
   goToNextPage() {
-    this.currentPageIndex = this.currentPageIndex + 1;
-    this.goToPage(this.currentPageIndex);
+    if (this.currentPageIndex !== this.pages.length - 1) {
+      this.currentPageIndex = this.currentPageIndex + 1;
+      this.goToPage(this.currentPageIndex);
+    }
+
   }
 
-  goToFirst(){
+  goToFirst() {
     this.currentPageIndex = 0;
     this.goToPage(this.currentPageIndex);
   }
 
-  goToLast(){
-   this.currentPageIndex = this.pages.length -1;
-   this.goToPage(this.currentPageIndex);
+  goToLast() {
+    this.currentPageIndex = this.pages.length - 1;
+    this.goToPage(this.currentPageIndex);
   }
 }
